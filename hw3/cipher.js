53229ae02648fd8ad521fd8c8f752be465ca55fe
Ievgeniia Ganzina <Ievgeniia_Ganzina@epam.com>
2019-12-04 23:11:05 +0200

    Refactored variable names

--- hw3/cipher.js
+++ hw3/cipher.js
@@ -2,27 +2,27 @@
 У вас є шматок тексту «This is my text for hometask”​
 Реалізуйте скрипт, який видозмінить цей рядок, застосувавши шифр Цезаря з кроком 2.​
 Тип циклу обирайте на свій смак */
- 
- 
+
+
 let text ="This is my text for hometask";
 let cipheredText = "";
 let step = 2;
 let alphabet= "abcdefghijklmnopqrstuvwxyz";
 
- 
-for (let i=0;i<text.length;i++){
-if(text.charAt(i)!=" "){
-   let place = alphabet.indexOf(text.toLowerCase().charAt(i));
-   let cipheredPlace = place+step;
 
-  
-   if(cipheredPlace>alphabet.length-1){
-       cipheredPlace = cipheredPlace - alphabet.length;
-   }
-   cipheredText+=alphabet.charAt(cipheredPlace);
-}
-else{
-    cipheredText+=" ";
-}
+for (let i=0;i<text.length;i++){
+	if(text.charAt(i)!==" "){
+		const currentAlphabetPosition = alphabet.indexOf(text.toLowerCase().charAt(i));
+		let cipheredAlphabetPosition = currentAlphabetPosition+step;
+
+
+		if(cipheredAlphabetPosition>alphabet.length-1){
+			cipheredAlphabetPosition = cipheredAlphabetPosition - alphabet.length;
+		}
+		cipheredText+=alphabet.charAt(cipheredAlphabetPosition);
+	}
+	else{
+		cipheredText+=" ";
+	}
 }
 console.log(cipheredText);
